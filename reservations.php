3b065183eb7aac82498e2eefe3731930595100d2
Khánh Huyền <huyentrinh.31221026063@st.ueh.edu.vn>
2025-05-30 11:44:59 +0700

    Update reservations.php

--- reservations.php
+++ reservations.php
@@ -28,7 +28,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $contact = $_POST['contact'];
     $noOfGuests = $_POST['noOfGuests'];
     $reservedTime = $_POST['reservedTime']; //-- Định dạng HH:MM
-    echo "Thời gian nhận từ form: " . $_POST['reservedTime'];
+    echo "Thời gian nhận từ form: " . $reservedTime;
+exit;
     $reservedDate = $_POST['reservedDate']; //-- Định dạng YYYY-MM-DD
 
     
